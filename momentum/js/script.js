959ea3d2de641e47c750bf9b3ad5dc89269e5ce2
Milsevka <lusindami99@gmail.com>
2022-07-30 14:21:21 +0300

    feat: add weather

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,6 +2,12 @@ const time = document.querySelector('.time');
 const dateMain = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const nameMain = document.querySelector('.name');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
 
 
 // 1.Часы и календарь
@@ -26,14 +32,14 @@ showDate()
 function showGreeting() {
     const date = new Date();
     const hour = date.getHours()
-    if (hour < 6) {
-        greeting.innerHTML = 'Good night, ';
-      } else if (hour < 12) {
+    if (hour < 12) {
         greeting.innerHTML = 'Good morning, ';
-      } else if (hour < 18) {
-        greeting.innerHTML = 'Good day, ';
-      } else {
+      } else if (hour < 17) {
+        greeting.innerHTML = 'Good afternoon, ';
+      } else if (hour < 20) {
         greeting.innerHTML = 'Good evening, ';
+      } else {
+        greeting.innerHTML = 'Good night, ';
       }
     }
 showGreeting()
@@ -49,3 +55,48 @@ function getLocalStorage() {
     }
   }
 window.addEventListener('load', getLocalStorage)
+// 3. Слайдер
+
+
+// 4. Погода
+
+async function getWeather() {  
+  if (!city.value) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
+  }
+
+  const res = await fetch(url);
+  const dataW = await res.json(); 
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${dataW.weather[0].id}`);
+  temperature.textContent = `${Math.ceil(dataW.main.temp)}°C`;
+  weatherDescription.textContent = dataW.weather[0].description;
+  wind.textContent = `Wind speed: ${dataW.wind.speed} m/s`;
+  humidity.textContent = `Humidity: ${dataW.main.humidity} %`;
+}
+
+function setLocalStorageWeather() {
+  localStorage.setItem('city', city.value);
+}
+
+function getLocalStorageWeather() {
+  if(localStorage.getItem('city')) {
+    city.value = localStorage.getItem('city');
+  } else {
+    city.value = 'Minsk';
+  }
+  getWeather()
+}
+ function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+  }
+ }
+
+window.addEventListener('beforeunload', setLocalStorageWeather)
+window.addEventListener('load', getLocalStorageWeather)
+city.addEventListener('keydown', setCity);
+
+// 5. Виджет "цитата дня"
