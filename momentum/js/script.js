a268941b88bfa21561bed028c11476174adc88c1
Milsevka <lusindami99@gmail.com>
2022-08-03 03:18:13 +0300

    fr

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -13,6 +13,12 @@ const slideNext = document.querySelector('.slide-next');
 const quotez = document.querySelector('.quote');
 const authorz = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
+const buttonPlay = document.querySelector('.play');
+const playPrev = document.querySelector('.play-prew');
+const playNext = document.querySelector('.play-next');
+const player = document.querySelector('.player');
+
+
 
 // 1.Часы и календарь
 
@@ -37,16 +43,16 @@ function getTimeOfDay() {
     const date = new Date();
     const hour = date.getHours()
     const arrayDays = ['morning', 'afternoon', 'evening', 'night'];
-    if (hour < 12 && hour > 6)  {
+    if (hour >=6 && hour < 12 )  {
       greeting.textContent = `Good ${arrayDays[0]},`;
       return arrayDays[0];
-      } else if (hour < 18) {
+      } else if (hour >=12 && hour < 18) {
         greeting.textContent = `Good ${arrayDays[1]},`;
         return arrayDays[1];
-      } else if (hour < 24) {
+      } else if (hour >=18 && hour < 24) {
         greeting.textContent = `Good ${arrayDays[2]},`;
         return arrayDays[2];
-      } else {
+      } else if (hour >=0 && hour < 6) {
         greeting.textContent = `Good ${arrayDays[3]},`; 
         return arrayDays[3]
       }
@@ -108,22 +114,7 @@ slidePrev.addEventListener('click', getSlidePrev);
 
 // 4. Погода
 
-async function getWeather() {  
-  if (!city.value) {
-    url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
-  } else {
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
-  }
 
-  const res = await fetch(url);
-  const dataW = await res.json(); 
-  weatherIcon.className = 'weather-icon owf';
-  weatherIcon.classList.add(`owf-${dataW.weather[0].id}`);
-  temperature.textContent = `${Math.ceil(dataW.main.temp)}°C`;
-  weatherDescription.textContent = dataW.weather[0].description;
-  wind.textContent = `Wind speed: ${Math.ceil(dataW.wind.speed)} m/s`;
-  humidity.textContent = `Humidity: ${dataW.main.humidity} %`;
-}
 
 function setLocalStorageWeather() {
   localStorage.setItem('city', city.value);
@@ -143,6 +134,24 @@ function getLocalStorageWeather() {
   }
  }
 
+ async function getWeather() { 
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`; 
+  if (!city.value) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=4d15f3c7e3cf78bab2ecd92db3f8b6dc&units=metric`;
+  }
+
+  const res = await fetch(url);
+  const dataW = await res.json(); 
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${dataW.weather[0].id}`);
+  temperature.textContent = `${Math.ceil(dataW.main.temp)}°C`;
+  weatherDescription.textContent = dataW.weather[0].description;
+  wind.textContent = `Wind speed: ${Math.ceil(dataW.wind.speed)} m/s`;
+  humidity.textContent = `Humidity: ${dataW.main.humidity} %`;
+}
+
 window.addEventListener('beforeunload', setLocalStorageWeather)
 window.addEventListener('load', getLocalStorageWeather)
 city.addEventListener('keydown', setCity);
@@ -152,10 +161,72 @@ async function getQuotes() {
   const quotes = 'quotes.json';
   const res = await fetch(quotes);
   const data = await res.json(); 
-  let randomNumQuote = getRandomNum(0, Object.keys(data.quotes).length);
+  let randomNumQuote = getRandomNum(0, Object.keys(data.quotes).length-1); 
   authorz.textContent = data.quotes[randomNumQuote].author;
   quotez.textContent = data.quotes[randomNumQuote].quote
 }
 getQuotes();
 
 changeQuote.addEventListener('click', getQuotes);
+
+// 6. Плейлист
+ 
+import playList from './playList.js';
+const audio = new Audio();
+function playAudio() {
+  audio.src = playList[playNum].src;
+  audio.currentTime = 0;
+  audio.play();
+  player.classList.add('meow');
+}
+
+function pauseAudio() {
+  audio.pause();
+  player.classList.remove('meow');
+}
+
+function toggleBtn() {
+  buttonPlay.classList.toggle('pause');
+}
+
+let playNum = 0
+function plPrev () {
+  playNum--
+  if (playNum < 0 ) {
+    playNum = playList.length -1
+  }
+  playAudio()
+}
+
+function plNext () {
+playNum++
+if (playNum > playList.length -1 ) {
+  playNum = 0
+}
+playAudio()
+buttonPlay.classList.add('pause');
+}
+
+
+buttonPlay.addEventListener('click', () => {
+  const isPlay = player.classList.contains('meow')
+  if (isPlay) {
+    pauseAudio ()
+  } else {
+    playAudio()
+  }
+});
+
+
+buttonPlay.addEventListener('click', toggleBtn);
+playNext.addEventListener('click', plNext);
+playPrev.addEventListener('click', plPrev);
+
+
+
+
+
+
+
+
+
