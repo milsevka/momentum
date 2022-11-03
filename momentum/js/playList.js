edf168cc2743d45945be9140f414d47d99698bbb
Milsevka <104006026+milsevka@users.noreply.github.com>
2022-11-04 02:45:42 +0300

    Update playList.js

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
     {      
       title: 'Aqua Caelestis',
-      src: '../../milsevka-JSFEPRE2022Q2/blob/momentum/momentum/assets/sounds/Aqua Caelestis.mp3',
+      src: 'assets/sounds/AquaCaelestis.mp3',
       duration: '00:30'
     },  
     {      
       title: 'Ennio Morricone',
-      src: '../../milsevka-JSFEPRE2022Q2/blob/momentum/momentum/assets/sounds/Ennio Morricone.mp3',
+      src: 'assets/sounds/EnnioMorricone.mp3',
       duration: '00:30'
     },
     {      
      title: 'River Flows In You',
-      src: '../../milsevka-JSFEPRE2022Q2/blob/momentum/momentum/assets/sounds/River Flows In You.mp3',
+      src: 'assets/sounds/RiverFlowsInYou.mp3',
       duration: '00:30'
     },
     {      
         title: 'Summer Wind',
-        src: '../../milsevka-JSFEPRE2022Q2/blob/momentum/momentum/assets/sounds/Summer Wind.mp3',
+        src: 'SummerWind.mp3',
         duration: '00:30'
     }
   ]
